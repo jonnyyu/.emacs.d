e7e7f77225cf6707f2fb821c735ef451a84de0c5
Jonny Yu <jonny.yu@autodesk.com>
2017-11-23 08:55:26 +0800

    update

--- init.el
+++ init.el
@@ -1,19 +1,27 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'package)
 (package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://elpa.emacs-china.org/melpa/") t)
 
+
+;; Disable toolbar, scrollbar in GUI Emacs
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; Show line number in all modes
 (global-linum-mode t)
+
+;; Disable splash screen
 (setq inhibit-splash-screen t)
+
 
 (defun open-init-file ()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-init-file)
-
-(recentf-mode t)
 
 (global-company-mode t)
 
@@ -24,8 +32,55 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso)))
- '(package-selected-packages (quote (company))))
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(custom-enabled-themes (quote (atom-dark)))
+ '(custom-safe-themes
+   (quote
+    ("e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default)))
+ '(fci-rule-color "#3C3D37")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100))))
+ '(magit-diff-use-overlays nil)
+ '(package-selected-packages
+   (quote
+    (counsel swiper smex atom-dark-theme hungry-delete monokai-theme company)))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,3 +89,47 @@
  )
 
 (setq-default cursor-type 'bar)
+(setq make-backup-files nil)
+
+;; enable syntax highlighting in embeded code block
+(require 'org)
+(setq org-src-fontify-natively t)
+
+;; enable recentf mode
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 10)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; add delete selection mode, feels more natural
+(delete-selection-mode t)
+
+;; open with maximum window
+;; (setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; highlight matching paren
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; highlight current line
+(global-hl-line-mode t)
+
+;; hungry delete mode
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;; config smex
+(require 'smex)
+(smex-initialize)
+;; (global-set-key (kbd "M-x") 'smex)
+
+;; config swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
